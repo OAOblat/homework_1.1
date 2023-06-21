2ce8cbb2145f766708e9f7c02592f9545a827670
Olga Oblat <oa.oblat@gmail.com>
2023-06-21 17:41:03 +0300

    testNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,55 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(920);
+        int expected = 80;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmount0() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmountEqualsBoundaryMinus1() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1001);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+}
